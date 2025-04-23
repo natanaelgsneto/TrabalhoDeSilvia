bb2877eae58a9fac5d59ed1066484586a722ebc4
natanaelgsneto <natanaelguedessn@gmail.com>
2025-04-22 21:18:08 -0300

    Add files via upload

---
+++ GRÁFICOS/Graficos 2a tabela/soma de discentes2.R
@@ -0,0 +1,22 @@
+install.packages("ggplot2")
+library(ggplot2)
+
+# Vetores de exemplo
+nomes <- c("Municipal", "Federal", "Estadual", "Prisional")
+quantidades <- c(5, 0, 2, 0)
+
+# Data frame
+df <- data.frame(fruta = nomes, quantidade = quantidades)
+
+# Gráfico
+ggplot(df, aes(x = fruta, y = quantidade)) +
+  geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+  geom_text(aes(label = quantidade), vjust = -0.5, size = 4) +
+  labs(
+    title = "Soma de Discentes por Escolas",
+    x = "Tipos de Escolas",
+    y = "Frequência das Discentes"
+  ) +
+  scale_y_continuous(limits = c(0, 10)) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5))
